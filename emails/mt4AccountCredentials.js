d5b4317a0c6920795d6c1fd717643299bc9e7c74
margin-funding <Marginfundingnet@gmail.com>
2025-04-30 04:45:35 +0500

    Update mt4AccountCredentials.js

--- emails/mt4AccountCredentials.js
+++ emails/mt4AccountCredentials.js
@@ -28,8 +28,8 @@ async function mt4AccountCredentials(
       secure: true,
       secureConnection: false,
       auth: {
-        user: process.env.EMAIL_ADMIN,
-        pass: process.env.PASSWORD_ADMIN,
+        user: "marginfundingnet@gmail.com",
+        pass: "olvpdexagtpfzwps",
       },
       tls: {
         rejectUnauthorized: true,
