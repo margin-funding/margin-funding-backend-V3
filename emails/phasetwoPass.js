9b58a2c17035b40f943b09e0c25ddb4d86ba9dd3
margin-funding <Marginfundingnet@gmail.com>
2025-04-30 04:46:39 +0500

    Update phasetwoPass.js

--- emails/phasetwoPass.js
+++ emails/phasetwoPass.js
@@ -16,8 +16,8 @@ async function phaseTwoPass(receiver, name) {
       secure: true,
       secureConnection: false,
       auth: {
-        user: process.env.EMAIL_ADMIN,
-        pass: process.env.PASSWORD_ADMIN,
+        user: "marginfundingnet@gmail.com",
+        pass: "olvpdexagtpfzwps",
       },
       tls: {
         rejectUnauthorized: true,
